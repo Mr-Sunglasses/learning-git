e331ddab7ee285052303debdd89e9370f039ae1b
Kanishk Pachauri <krishna03012003@icloud.com>
2021-09-01 23:14:34 +0530

    added functionality of appending a list

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def reverse_list(list_input):
     return list_input[::-1]
 
+def append_list(list_name,word):
+    return list_name.append(word)
+my_list = [1,2,3,4,5,9,8,0,36,11]
 
-my_list = [1,2,3,4,5]
 print(reverse_list(my_list))
+append_list(my_list,100)
+print(my_list)
+
