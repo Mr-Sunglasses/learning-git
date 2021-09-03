07167fb75ce5d8fbc64b777ecd958f1d96424da8
Kanishk Pachauri <krishna03012003@icloud.com>
2021-09-03 10:07:26 +0530

    removed printing 🐧 of list in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def append_list(list_name,word):
     return list_name.append(word)
 my_list = [1,2,3,4,5,9,8,0,36,11]
 
-print(reverse_list(my_list))
+#print(reverse_list(my_list))
 append_list(my_list,100)
-print(my_list)
+#print(my_list)
 
